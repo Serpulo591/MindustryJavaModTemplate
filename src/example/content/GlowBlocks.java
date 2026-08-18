fb3d2e31bf0ac0054f319a6619b7cc30940ac56c
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 22:26:11 +0800

    Update GlowBlocks.java

--- src/example/content/GlowBlocks.java
+++ src/example/content/GlowBlocks.java
@@ -1,40 +1,70 @@
 package example.content;
 
-import mindustry.world.Block;
-import mindustry.world.blocks.distribution.Conveyor;
-import mindustry.world.blocks.production.Drill;
-import mindustry.type.ItemStack;
-import mindustry.content.Items;
-import mindustry.content.Liquids;     // 改用 content 包
-import example.content.GlowItems;
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import mindustry.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.DrawPart.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.unit.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.heat.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+import static mindustry.type.ItemStack.*;
 
 public class GlowBlocks {
     public static Block
         conveyor1,
-        highCarbonConveyor,
+        highCarbonConveyor;
         vectorDrill;
 
     public static void load() {
         conveyor1 = new Conveyor("conveyor1") {{
-            requirements(Block.Category.distribution, new ItemStack[]{new ItemStack(GlowItems.cobalt, 1)});
+            requirements(Category.distribution, with(Items.cobalt, 1));
             health = 45;
             speed = 0.06f;
             displayedSpeed = 8.4f;
             buildCostMultiplier = 2;
-            researchCost = new ItemStack[]{new ItemStack(GlowItems.cobalt, 10)};
+            researchCost = with(Items.cobalt, 10);
         }};
 
         highCarbonConveyor = new Conveyor("high-carbon-conveyor") {{
-            requirements(Block.Category.distribution, new ItemStack[]{
-                new ItemStack(GlowItems.cobalt, 3),
-                new ItemStack(Items.titanium, 2),
-                new ItemStack(GlowItems.highCarbonAlloy, 2)
-            });
+            requirements(Category.distribution, with(Items.cobalt, 3, Items.titanium, 2, Items.high-carbon-alloy, 2));
             health = 400;
             speed = 0.2036363636f;
             displayedSpeed = 28;
         }};
-
+        
+        //占位符
+        
         vectorDrill = new Drill("vector-drill") {{
             researchCostMultiplier = 0.1f;
             size = 2;
@@ -45,10 +75,7 @@ public class GlowBlocks {
             warmupSpeed = 30f;
             itemCapacity = 10;
             liquidCapacity = 38f;
-            requirements(Block.Category.production, new ItemStack[]{
-                new ItemStack(GlowItems.cobalt, 12),
-                new ItemStack(Items.tungsten, 8)
-            });
+            requirements(Category.production, with(Items.cobalt, 12, Items.tungsten, 8));
             consumePower(0.5f);
             consumeLiquid(Liquids.water, 0.065f).boost();
         }};
