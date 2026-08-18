df7b7fb1fd06f08f71ca7dc0b798b7e3fd72d185
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 17:07:56 +0800

    Update and rename Cobalt.java to GlowItem.java

---
+++ src/example/content/GlowItem.java
@@ -0,0 +1,29 @@
+package example.content;
+
+import mindustry.type.Item;
+
+public class item {
+    public static Item 
+        cobalt,
+        crudeSilicon,
+        highCarbonAlloy;
+
+    public void load(){
+        cobalt = new Item(name: "cobalt", Color.valueOf("3a6ea5")){{
+            hardness = 1;
+            cost = 1;
+            alwaysUnlocked = true;
+        }};
+        
+        crudeSilicon = new Item(name: "crude-silicon", Color.valueOf("676c70"){{
+            buildable = false;
+            alwaysUnlocked = false;
+        }};
+        
+        highCarbonAlloy = new Item(name: "high-carbon-alloy", Color.valueOf("3a6ea5")){{
+            radioactivity = 0.3f;
+            cost = 3;
+            alwaysUnlocked = false;
+        }};
+    }
+}
