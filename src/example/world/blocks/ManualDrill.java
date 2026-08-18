77fd58480d1d598f04fa4a681ebcd40c1747311a
Serpulo591 <james.whitmore689@gmail.com>
2026-08-19 00:46:05 +0800

    Update ManualDrill.java

--- src/example/world/blocks/ManualDrill.java
+++ src/example/world/blocks/ManualDrill.java
@@ -1,49 +1,19 @@
 package example.world.blocks;
 
-// 完全复制 GlowBlocks 的导入列表
-import arc.graphics.*;
-import arc.math.*;
-import arc.struct.*;
-import mindustry.*;
-import mindustry.entities.*;
-import mindustry.entities.abilities.*;
-import mindustry.entities.bullet.*;
-import mindustry.entities.effect.*;
-import mindustry.entities.part.DrawPart.*;
-import mindustry.entities.part.*;
-import mindustry.entities.pattern.*;
-import mindustry.game.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.type.*;
-import mindustry.type.unit.*;
-import mindustry.world.*;
-import mindustry.world.blocks.*;
-import mindustry.world.blocks.campaign.*;
-import mindustry.world.blocks.defense.*;
-import mindustry.world.blocks.defense.turrets.*;
-import mindustry.world.blocks.distribution.*;
-import mindustry.world.blocks.environment.*;
-import mindustry.world.blocks.heat.*;
-import mindustry.world.blocks.legacy.*;
-import mindustry.world.blocks.liquid.*;
-import mindustry.world.blocks.logic.*;
-import mindustry.world.blocks.payloads.*;
-import mindustry.world.blocks.power.*;
-import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.sandbox.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.blocks.units.*;
-import mindustry.world.consumers.*;
-import mindustry.world.draw.*;
-import mindustry.world.meta.*;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.math.Mathf;
+import mindustry.gen.Sounds;
+import mindustry.graphics.Pal;
+import mindustry.world.blocks.production.Drill;
+import mindustry.type.ItemStack;
 import mindustry.content.Items;
 import mindustry.content.Liquids;
+import mindustry.world.blocks.production.Drill.DrillBuild;
+import example.content.GlowItems;
+
 import static mindustry.Vars.*;
 import static mindustry.type.ItemStack.*;
-
-// 导入你的自定义物品
-import example.content.GlowItems;
 
 public class ManualDrill extends Drill {
 
@@ -59,7 +29,6 @@ public class ManualDrill extends Drill {
         warmupSpeed = 30f;
         itemCapacity = 10;
         liquidCapacity = 38f;
-        // 现在 Category 和 with 都有了
         requirements(Category.production, with(GlowItems.cobalt, 12, Items.tungsten, 8));
         consumePower(0.5f);
         consumeLiquid(Liquids.water, 0.065f).boost();
@@ -102,11 +71,13 @@ public class ManualDrill extends Drill {
 
             Sounds.click.play();
 
-            // 点击特效：用 Draw 和 Fill，不用任何外部类
+            // 点击特效：直接画彩色圆点
             Draw.color(Pal.accent);
             Fill.circle(this.x, this.y, 3f);
             Draw.color(Pal.ammo);
             Fill.circle(this.x - 2, this.y - 2, 2f);
+            Draw.color(Pal.accent);
+            Fill.circle(this.x + 2, this.y + 2, 1.5f);
         }
     }
 }
