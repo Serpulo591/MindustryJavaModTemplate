ee0dbc1a25e6bd4acf50a4ffc81dfb6dcc119341
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 23:05:55 +0800

    Create ManualDrill.java

---
+++ src/example/world/blocks/ManualDrill.java
@@ -0,0 +1,76 @@
+package example.content;
+
+import mindustry.world.blocks.production.Drill;
+import mindustry.world.meta.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.Items;
+import mindustry.type.Liquids;
+import mindustry.gen.Sounds;
+import mindustry.graphics.Fx;
+import mindustry.math.Mathf;
+import mindustry.world.blocks.production.Drill.DrillBuild;
+
+import static mindustry.Vars.with;
+
+public class ManualDrill extends Drill {
+
+    public ManualDrill(String name) {
+        super(name);
+
+        // 基础属性
+        researchCostMultiplier = 0.1f;
+        size = 2;
+        health = 300;
+        tier = 100;
+        drillTime = 180;
+        canOverdrive = true;
+        warmupSpeed = 30f;
+        itemCapacity = 10;
+        liquidCapacity = 38f;
+        requirements(Category.production, with(GlowItems.cobalt, 12, Items.tungsten, 8));
+        consumePower(0.5f);
+        consumeLiquid(Liquids.water, 0.065f).boost();
+
+        // 自定义建筑逻辑
+        buildType = () -> new ManualDrillBuild();
+    }
+
+    // 内部类：自定义钻头逻辑
+    public static class ManualDrillBuild extends DrillBuild {
+        public float boostLevel = 1f;
+        public int emergencyTime = 0;
+        public final float boostAdd = 1f;
+        public final float boostMax = 20f;
+        public final float boostRecover = 0.02f;
+
+        @Override
+        public void updateTile() {
+            if (boostLevel > 1f) {
+                boostLevel = Mathf.lerpDelta(boostLevel, 1f, boostRecover);
+            }
+            this.timeScale = boostLevel;
+
+            if (emergencyTime > 0) {
+                emergencyTime--;
+                if (this.power.status <= 0) {
+                    this.efficiency = 1f;
+                }
+            }
+
+            super.updateTile();
+        }
+
+        @Override
+        public void tapped() {
+            boostLevel += boostAdd;
+            if (boostLevel > boostMax) boostLevel = boostMax;
+
+            if (this.power.status <= 0) {
+                emergencyTime = 60;
+            }
+
+            Sounds.click.play();
+            Fx.mineSmall.at(this.x, this.y);
+        }
+    }
+}
