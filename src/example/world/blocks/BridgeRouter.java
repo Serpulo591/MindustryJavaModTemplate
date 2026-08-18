66c16fde8844c201b611d37845d1437fdf0b0d17
Serpulo591 <james.whitmore689@gmail.com>
2026-08-19 03:27:00 +0800

    Add files via upload

--- src/example/world/blocks/BridgeRouter.java
+++ src/example/world/blocks/BridgeRouter.java
@@ -21,8 +21,6 @@ import mindustry.ui.Bar;
 import mindustry.world.blocks.storage.StorageBlock;
 import mindustry.world.meta.Stat;
 import mindustry.world.meta.StatUnit;
-import mindustry.game.Events;
-import mindustry.game.EventType.Trigger;
 
 import static mindustry.Vars.*;
 
@@ -51,11 +49,6 @@ public class BridgeRouter extends StorageBlock {
 
     // 全局活跃桥列表
     public static final Seq<BridgeBuild> activeBridges = new Seq<>();
-
-    // 静态初始化：注册绘制事件
-    public static void registerEvents() {
-        Events.run(Trigger.draw, BridgeBuild::drawAllBridges);
-    }
 
     public BridgeRouter(String name) {
         super(name);
