bb3d2c5d01337004446f222274d3e8292e2e1658
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 16:31:03 +0800

    Update ModItem.java

--- src/example/content/ModItem.java
+++ src/example/content/ModItem.java
@@ -1,4 +1,4 @@
-package example;   // ← 这一行必须加，和 ExampleJavaMod 在同一个包
+package example;
 
 import mindustry.type.Item;
 
@@ -6,9 +6,10 @@ public class ModItem {
     public static Item cobalt;
 
     public static void load() {
-        cobalt = new Item("cobalt") {{
+        cobalt = new Item("cobalt", Color.valueOf("3a6ea5")) {{
             hardness = 1;
             cost = 1;
+            research = "core-origin";           // 对应 JSON 的 research
         }};
     }
 }
