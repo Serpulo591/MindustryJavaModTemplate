48794bcf9dd193fa45d234402d187043e60319b2
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 17:31:56 +0800

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -8,20 +8,20 @@ import mindustry.ui.dialogs.*;
 
 import example.content.GlowItems;
 
-public class ExampleJavaMod extends Mod{
-    public ExampleJavaMod(){
-        Events.on(EventType.ClientLoadEvent.class, e-> {
-            Time.run(delay: 10f, () -> {
-                BaseDialog dialog = new BaseDialog(title: "Welcome");
+public class ExampleJavaMod extends Mod {
+    public ExampleJavaMod() {
+        Events.on(ClientLoadEvent.class, e -> {
+            Time.run(10f, () -> {
+                BaseDialog dialog = new BaseDialog("Welcome");
                 dialog.cont.add("No Welcome");
-                Time.run(delay: 100f, dialog::addCloseButton);
+                Time.run(100f, dialog::addCloseButton);
                 dialog.show();
             });
         });
     }
 
     @Override
-    public void loadContent(){
+    public void loadContent() {
         GlowItems.load();
     }
 }
