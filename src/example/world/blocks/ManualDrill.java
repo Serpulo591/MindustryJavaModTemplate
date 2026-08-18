b997710a2eabfa07bf7d9f4aa332d817352b8909
Serpulo591 <james.whitmore689@gmail.com>
2026-08-19 01:20:03 +0800

    Update ManualDrill.java

--- src/example/world/blocks/ManualDrill.java
+++ src/example/world/blocks/ManualDrill.java
@@ -2,49 +2,20 @@ package example.world.blocks;
 
 import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.Fill;
-import arc.graphics.*;
-import arc.math.*;
-import arc.struct.*;
-import mindustry.*;
-import mindustry.entities.*;
-import mindustry.entities.abilities.*;
-import mindustry.entities.bullet.*;
-import mindustry.entities.effect.*;
-import mindustry.entities.part.DrawPart.*;
-import mindustry.entities.part.*;
-import mindustry.entities.pattern.*;
-import mindustry.game.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.type.*;
-import mindustry.type.unit.*;
-import mindustry.world.*;
-import mindustry.world.blocks.*;
-import mindustry.world.blocks.campaign.*;
-import mindustry.world.blocks.defense.*;
-import mindustry.world.blocks.defense.turrets.*;
-import mindustry.world.blocks.distribution.*;
-import mindustry.world.blocks.environment.*;
-import mindustry.world.blocks.heat.*;
-import mindustry.world.blocks.legacy.*;
-import mindustry.world.blocks.liquid.*;
-import mindustry.world.blocks.logic.*;
-import mindustry.world.blocks.payloads.*;
-import mindustry.world.blocks.power.*;
-import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.sandbox.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.blocks.units.*;
-import mindustry.world.consumers.*;
-import mindustry.world.draw.*;
-import mindustry.world.meta.*;
+import arc.math.Mathf;
+import mindustry.gen.Sounds;
+import mindustry.graphics.Pal;
+import mindustry.world.blocks.production.Drill;
+import mindustry.type.ItemStack;
 import mindustry.content.Items;
 import mindustry.content.Liquids;
+import mindustry.world.blocks.production.Drill.DrillBuild;
+import example.content.GlowItems;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
 
 import static mindustry.Vars.*;
 import static mindustry.type.ItemStack.*;
-
-import example.content.GlowItems;
 
 public class ManualDrill extends Drill {
 
@@ -60,7 +31,6 @@ public class ManualDrill extends Drill {
         warmupSpeed = 30f;
         itemCapacity = 10;
         liquidCapacity = 38f;
-        requirements(Category.production);
         consumePower(0.5f);
         consumeLiquid(Liquids.water, 0.065f).boost();
 
