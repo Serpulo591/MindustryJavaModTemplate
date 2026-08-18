f39c339c3abf8a8a3e115e99650bbf3c7ac74d43
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 17:31:32 +0800

    Update GlowItems.java

--- src/example/content/GlowItems.java
+++ src/example/content/GlowItems.java
@@ -1,26 +1,27 @@
 package example.content;
 
 import mindustry.type.Item;
+import arc.graphics.Color;
 
-public class item {
-    public static Item 
+public class GlowItems {
+    public static Item
         cobalt,
         crudeSilicon,
         highCarbonAlloy;
 
-    public static void load(){
-        cobalt = new Item(name: "cobalt", Color.valueOf("3a6ea5")){{
+    public static void load() {
+        cobalt = new Item("cobalt", Color.valueOf("3a6ea5")) {{
             hardness = 1;
             cost = 1;
             alwaysUnlocked = true;
         }};
-        
-        crudeSilicon = new Item(name: "crude-silicon", Color.valueOf("676c70")){{
+
+        crudeSilicon = new Item("crude-silicon", Color.valueOf("676c70")) {{
             buildable = false;
             alwaysUnlocked = false;
         }};
-        
-        highCarbonAlloy = new Item(name: "high-carbon-alloy", Color.valueOf("4a5a6a")){{
+
+        highCarbonAlloy = new Item("high-carbon-alloy", Color.valueOf("4a5a6a")) {{
             radioactivity = 0.3f;
             cost = 3;
             alwaysUnlocked = false;
