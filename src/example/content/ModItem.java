71d43ea4a746ef1a53de17459f931172645c6e69
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 14:19:56 +0800

    Add files via upload

--- src/example/content/ModItem.java
+++ src/example/content/ModItem.java
@@ -1,7 +1,10 @@
-public static Item cobalt;
-public static void load(){
-  cobalt = new Item("cobalt"){{
-    hardness = 1;
-    cost = 1;
-  }};
-};
+public class ModItem {   // ← 这栋“房子”必须存在
+    public static Item cobalt;
+
+    public static void load() {
+        cobalt = new Item("cobalt") {{
+            hardness = 1;
+            cost = 1;
+        }};
+    }
+}   // ← 房子关好
