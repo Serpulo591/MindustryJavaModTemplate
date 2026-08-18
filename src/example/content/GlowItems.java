fca2439048e1bee6c203dff953b73515c881068c
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 17:15:08 +0800

    Change load method to static and update item color

--- src/example/content/GlowItems.java
+++ src/example/content/GlowItems.java
@@ -8,7 +8,7 @@ public class item {
         crudeSilicon,
         highCarbonAlloy;
 
-    public void load(){
+    public static void load(){
         cobalt = new Item(name: "cobalt", Color.valueOf("3a6ea5")){{
             hardness = 1;
             cost = 1;
@@ -20,7 +20,7 @@ public class item {
             alwaysUnlocked = false;
         }};
         
-        highCarbonAlloy = new Item(name: "high-carbon-alloy", Color.valueOf("3a6ea5")){{
+        highCarbonAlloy = new Item(name: "high-carbon-alloy", Color.valueOf("4a5a6a")){{
             radioactivity = 0.3f;
             cost = 3;
             alwaysUnlocked = false;
