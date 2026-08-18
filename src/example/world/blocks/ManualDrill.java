c5d91650fb2494098fe2272091a00833230b70eb
Serpulo591 <james.whitmore689@gmail.com>
2026-08-18 23:57:09 +0800

    Refactor ManualDrill.java by removing unused imports
    Removed unused imports to clean up the code.

--- src/example/world/blocks/ManualDrill.java
+++ src/example/world/blocks/ManualDrill.java
@@ -1,47 +1,28 @@
 package example.world.blocks;
 
+import arc.*;
 import arc.graphics.*;
+import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.struct.*;
-import mindustry.*;
+import arc.util.*;
+import arc.util.io.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.content.*;
 import mindustry.entities.*;
-import mindustry.entities.abilities.*;
-import mindustry.entities.bullet.*;
-import mindustry.entities.effect.*;
-import mindustry.entities.part.DrawPart.*;
-import mindustry.entities.part.*;
-import mindustry.entities.pattern.*;
+import mindustry.entities.units.*;
 import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import mindustry.logic.*;
 import mindustry.type.*;
-import mindustry.type.unit.*;
+import mindustry.ui.*;
 import mindustry.world.*;
-import mindustry.world.blocks.*;
-import mindustry.world.blocks.campaign.*;
-import mindustry.world.blocks.defense.*;
-import mindustry.world.blocks.defense.turrets.*;
-import mindustry.world.blocks.distribution.*;
 import mindustry.world.blocks.environment.*;
-import mindustry.world.blocks.heat.*;
-import mindustry.world.blocks.legacy.*;
-import mindustry.world.blocks.liquid.*;
-import mindustry.world.blocks.logic.*;
-import mindustry.world.blocks.payloads.*;
-import mindustry.world.blocks.power.*;
-import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.sandbox.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.blocks.units.*;
 import mindustry.world.consumers.*;
-import mindustry.world.draw.*;
 import mindustry.world.meta.*;
-import mindustry.content.Items;
-import mindustry.content.Liquids;
-import static mindustry.Vars.*;
-import static mindustry.type.ItemStack.*;
 
-import example.content.GlowItems;
+import static mindustry.Vars.*;
 
 public class ManualDrill extends Drill {
 
