b16d7a3fb783cf12a3e1765cb08e835fd2a48401
Serpulo591 <james.whitmore689@gmail.com>
2026-08-19 14:02:27 +0800

    Add files via upload

--- src/example/world/blocks/BridgeRouter.java
+++ src/example/world/blocks/BridgeRouter.java
@@ -105,7 +105,7 @@ public class BridgeRouter extends StorageBlock {
         super.setBars();
         addBar("connections", (BridgeRouterBuild e) ->
             new Bar(
-                () -> Vars.core.bundle.format("bar.powerlines", e.getLink().size, linkLimit),
+                () -> "Connections: " + e.getLink().size + "/" + linkLimit,
                 () -> Pal.accent,
                 () -> (float) e.getLink().size / linkLimit
             )
