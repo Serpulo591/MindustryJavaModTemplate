ca2442746ab2f82dc1be6b01cdb77b6bff0a046b
Serpulo591 <james.whitmore689@gmail.com>
2026-08-19 18:28:24 +0800

    Add files via upload

--- src/example/world/blocks/BridgeRouter.java
+++ src/example/world/blocks/BridgeRouter.java
@@ -3,8 +3,37 @@ package example.world.blocks;
 import mindustry.world.blocks.storage.StorageBlock;
 
 public class BridgeRouter extends StorageBlock {
+    public final int timerCheckMoved = timers ++;
+    public int range;
+    public float transportTime;
+    public boolean fadeIn = true;
+    public boolean moveArrows = true;
+    public boolean pulse = false;
+    public float arrowSpacing = 4f, arrowOffset = 2f, arrowPeriod = 0.4f;
+    public float arrowTimeScl = 6.2f;
+    public float bridgeWidth = 6.5f;
+    
+    public @Nullable BridgeRouterBuild lastBuild;
 
     public BridgeRouter(String name){
         super(name);
+        update = true;
+        solid = true;
+        underBullets = true;
+        hasPower = true;
+        itemCapacity = 10;
+        configurable = true;
+        hasItems = true;
+        unloadable = false;
+        group = BlockGroup.transportation;
+        noUpdateDisabled = true;
+        allowDiagonal = false;
+        copyConfig = false;
+        allowConfigInventory = false;
+        ignoreResizeConfig = true;
+        priority = TargetPriority.transport;
+        delayLandingConfig = true;
+        config(Point2.class, (ItemBridgeBuild tile, Point2 i) -> tile.link = Point2.pack(i.x + tile.tileX(), i.y + tile.tileY()));
+        config(Integer.class, (ItemBridgeBuild tile, Integer i) -> tile.link = i);
     }
 }
