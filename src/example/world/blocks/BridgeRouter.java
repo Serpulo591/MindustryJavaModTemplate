eafc0bb1116466a8fd10715e6953eeb2701dbe9f
Serpulo591 <james.whitmore689@gmail.com>
2026-08-19 18:47:57 +0800

    Add files via upload

--- src/example/world/blocks/BridgeRouter.java
+++ src/example/world/blocks/BridgeRouter.java
@@ -58,8 +58,8 @@ public class BridgeRouter extends StorageBlock {
         delayLandingConfig = true;
         config(Point2.class, (BridgeRouterBuild tile, Point2 i) -> tile.link = Point2.pack(i.x + tile.tileX(), i.y + tile.tileY()));
         config(Integer.class, (BridgeRouterBuild tile, Integer i) -> tile.link = i);
-        public static class BridgeRouterBuild extends StorageBuild {
-            public int link;
-        }
+    }
+    public static class BridgeRouterBuild extends StorageBuild {
+        public int link;
     }
 }
